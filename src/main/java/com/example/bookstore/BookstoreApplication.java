823eefef3f1fb8c8943bb8f747856f7c2eb23c30
goluart <artur-g@outlook.com>
2023-03-03 18:10:30 +0200

    Added spring security

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.bookstore.domain.AppUser;
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
+import com.example.bookstore.domain.AppUserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -22,7 +24,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, AppUserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			crepository.save(new Category("Fantasy"));
@@ -31,6 +33,11 @@ public class BookstoreApplication {
 			
 			repository.save(new Book("A Farewell to Arms", "Ernest Hemingway", "1929", "1232323-21", "35", crepository.findByName("Mystery").get(0)));
 			repository.save(new Book("Animal Farm", "George Orwell", "1945", "2212343-5", "30", crepository.findByName("Fantasy").get(0)));
+
+			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user1@hotmail.com");
+			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "user2@myy.haaga-helia.fi");
+			urepository.save(user1);
+			urepository.save(user2);
 
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -23,6 +23,11 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 
+	@RequestMapping(value="/login")
+	public String login() {
+		return "login";
+	}
+
 	@RequestMapping(value = { "/", "/booklist" })
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
